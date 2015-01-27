5c3143fcbbaac59e7e4fa757dbd598dd37a90db5
Fabian Thorand <f.thorand@gmail.com>
2015-01-27 13:22:09 +0100

    `UnFair` wrapper for `MonadLogic` implementations

--- code/denotational-funlogic/src/Control/Monad/Logic/Class/Extended.hs
+++ code/denotational-funlogic/src/Control/Monad/Logic/Class/Extended.hs
@@ -1,20 +1,33 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Control.Monad.Logic.Class.Extended where
 
 import Control.Applicative
+import Control.Arrow (second)
 import Control.Monad
 import Control.Monad.Logic.Class
 
--- | Like mapM, but with fair conjunction
+-- | Takes any monad with a 'MonadLogic' instance, but replaces its 'interleave' and '>>-' implementation
+-- by 'mplus' and '>>=', thus using the default MonadPlus behavior (which in many cases is depth-first search)
+-- even when using those 'MonadLogic' combinators.
+newtype UnFair m a = UnFair { fair :: m a }
+  deriving (Functor, Applicative, Alternative, Monad, MonadPlus)
+
+instance MonadLogic m => MonadLogic (UnFair m) where
+  msplit = UnFair . (liftM.liftM) (second UnFair) . msplit . fair
+  interleave = mplus
+  (>>-) = (>>=)
+
+-- | Like 'mapM', but with fair conjunction
 mapFairM :: MonadLogic m => (a -> m b) -> [a] -> m [b]
 mapFairM f = go where
   go []     = return []
   go (x:xs) = f x >>- \b -> (b:) `liftM` go xs
 
--- | Like `msum`, but with fair disjunction.
+-- | Like 'msum', but with fair disjunction.
 interleaveMany :: MonadLogic m => [m a] -> m a
 interleaveMany = foldr interleave mzero
 
--- | Like `liftM`, but with fair conjunction.
+-- | Like 'liftM', but with fair conjunction.
 liftFairM2 :: MonadLogic m => (a -> b -> c) -> m a -> m b -> m c
 liftFairM2 f = fairBind2 (\a b -> return $ f a b)
 
