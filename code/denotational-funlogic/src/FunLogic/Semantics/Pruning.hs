1235f29bd92361c87c6188eb3a6e6ffe84f4fd3a
Fabian Thorand <f.thorand@gmail.com>
2015-02-11 22:22:27 +0100

    Implemented a general function for pruning monadic computations returning elements of a partial order. (see #1)

---
+++ code/denotational-funlogic/src/FunLogic/Semantics/Pruning.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE LambdaCase #-}
+module FunLogic.Semantics.Pruning where
+
+import           Control.Applicative
+import           Control.Monad
+
+import qualified FunLogic.Semantics.PartialOrder as PO
+import qualified FunLogic.Semantics.Search       as Search
+
+-- | Removes a value from the monadic computation, if a larger value has already been returned.
+pruneNonMaximal :: (PO.PartialOrd a, Search.MonadSearch m) => m a -> m a
+pruneNonMaximal = pruneNonMaximal' id
+
+-- | Removes a value from the monadic computation, if a larger value has already been returned.
+-- Only the 'N' most recent maximal values are considered.
+pruneNonMaximalN :: (PO.PartialOrd a, Search.MonadSearch m) => Int -> m a -> m a
+pruneNonMaximalN n = pruneNonMaximal' (take n)
+
+-- | Removes a value from the monadic computation, if a larger value has already been returned.
+-- This functions allows to transform the internal representation of the list of maximal elements.
+pruneNonMaximal' :: (PO.PartialOrd a, Search.MonadSearch m) => ([a] -> [a]) -> m a -> m a
+pruneNonMaximal' process = go [] where
+  -- | Maintains a list of maximal elements encountered so far. It maintains the invariant, that each element in this
+  -- list is incomparable to every other element.
+  go maxvals comp = Search.peek comp >>= \case
+    Nothing -> mzero
+    Just (v,rest) -> case check maxvals v of
+      Nothing -> go maxvals rest
+      Just maxvals' -> return v <|> go maxvals' rest
+
+  -- | Checks if there is a larger or equal element in the list.
+  -- If it's not, all elements that are less than the given value are removed.
+  --
+  check xs v = (v:) <$> process <$> check' xs v
+
+  check' [] _       = Just []
+  check' (x:xs) v
+    | v `PO.leq` x = Nothing
+    | x `PO.lt`  v = check xs v
+    | otherwise    = (x:) <$> check xs v
--- code/denotational-funlogic/src/FunLogic/Semantics/Search.hs
+++ code/denotational-funlogic/src/FunLogic/Semantics/Search.hs
@@ -1,23 +1,42 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving, FlexibleInstances #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase                 #-}
 module FunLogic.Semantics.Search where
 
-import Control.Applicative
-import Control.Monad
-import Control.Monad.Reader
-import qualified Control.Monad.Logic as Logic
+import           Control.Applicative
+import           Control.Monad
+import qualified Control.Monad.Logic  as Logic
+import           Control.Monad.Reader
 
 -- | Monads supporting non-deterministic fair search.
 -- This custom search class is needed, because the 'MonadLogic' instance of 'ReaderT' does not respect the overriden
 -- implementations of 'interleave' and '>>-' in the base monad.
 class (Alternative m, MonadPlus m) => MonadSearch m where
+  -- | "Peek" at one result at a time.
+  peek :: m a -> m (Maybe (a, m a))
   -- | Fair disjunction between two branches
   branch :: m a -> m a -> m a
   -- | Fair conjunction.
   (>>?)  :: m a -> (a -> m b) -> m b
 
 instance Monad m => MonadSearch (Logic.LogicT m) where
-    branch = Logic.interleave
-    (>>?)  = (Logic.>>-)
+  peek   = Logic.msplit
+  branch = Logic.interleave
+  (>>?)  = (Logic.>>-)
+
+instance MonadSearch m => MonadSearch (ReaderT s m) where
+  peek rm = ReaderT $ \e -> do
+    r <- peek $ runReaderT rm e
+    case r of
+      Nothing -> return Nothing
+      Just (a, m) -> return (Just (a, lift m))
+
+  branch ma mb = ReaderT $ \r ->
+    runReaderT ma r `branch` runReaderT mb r
+
+  ma >>? f = ReaderT $ \s ->
+    runReaderT ma s >>? \a -> runReaderT (f a) s
+
 
 -- | A class of non-deterministic monads with observable results.
 class Observable m where
@@ -47,7 +66,13 @@ instance Observable [] where
 newtype UnFair m a = UnFair { fair :: m a }
   deriving (Functor, Applicative, Alternative, Monad, MonadPlus)
 
-instance (Alternative m, MonadPlus m) => MonadSearch (UnFair m) where
+instance MonadTrans UnFair where
+  lift = UnFair
+
+instance (Alternative m, MonadPlus m, MonadSearch m) => MonadSearch (UnFair m) where
+  peek rm = UnFair $ peek (fair rm) >>= \case
+    Nothing -> return Nothing
+    Just (a, m) -> return (Just (a, lift m))
   branch = mplus
   (>>?) = (>>=)
 
@@ -55,13 +80,6 @@ instance Observable m => Observable (UnFair m) where
   observe       = observe . fair
   observeAll    = observeAll . fair
   observeMany n = observeMany n . fair
-
-instance MonadSearch m => MonadSearch (ReaderT s m) where
-    branch ma mb = ReaderT $ \r ->
-                        runReaderT ma r `branch` runReaderT mb r
-
-    ma >>? f = ReaderT $ \s ->
-                runReaderT ma s >>? \a -> runReaderT (f a) s
 
 -- | Like 'mapM', but with fair conjunction
 mapFairM :: MonadSearch m => (a -> m b) -> [a] -> m [b]
