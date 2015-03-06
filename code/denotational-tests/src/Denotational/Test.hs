963371e086ad0d1855dbc10f2b3effda7e367ff0
Fabian Thorand <f.thorand@gmail.com>
2015-03-06 17:54:49 +0100

    Added more tests.

--- code/denotational-tests/src/Denotational/Test.hs
+++ code/denotational-tests/src/Denotational/Test.hs
@@ -1,2 +1,9 @@
+{-# LANGUAGE FlexibleContexts       #-}
+{-# LANGUAGE FlexibleInstances      #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE GADTs                  #-}
+{-# LANGUAGE KindSignatures         #-}
+{-# LANGUAGE LambdaCase             #-}
+{-# LANGUAGE MultiParamTypeClasses  #-}
+{-# LANGUAGE TypeFamilies           #-}
 module Denotational.Test where
-
